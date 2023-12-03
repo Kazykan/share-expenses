3d32eb97333cc26a386ce9332802ab01bf329a33
Rufat <rufat@kocherbaev.ru>
2023-12-03 08:20:48 +0300

    add calendar, but not fix it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { PlaceIdContext } from "./components/PlaceIdContext"
 import ProgressBar from "./components/ProgressBar"
 import PaymentService from "./components/PaymentService"
 import TransferMoney from "./components/TransferMoney"
+import Calendar from "./components/calendar/calendar"
 
 interface PlaceProps {
   place: IPlace
@@ -182,6 +183,32 @@ function App() {
             />
           </div>
         </div>
+
+        {/* Form */}
+
+        <form className="max-w-sm mx-auto">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Select your country
+          </label>
+          <select
+            id="countries"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option>United States</option>
+            <option>Canada</option>
+            <option>France</option>
+            <option>Germany</option>
+          </select>
+        </form>
+
+        {/* Form */}
+
+{/* Calendar */}
+
+<Calendar />
+
+{/* Calendar */}
+
 
         {/* Table  */}
         {/* Table  */}
