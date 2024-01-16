f9a8f3d10e2995dd31541fbb051411cde9ed8012
Rufat <rufat@kocherbaev.ru>
2024-01-16 14:33:05 +0300

    fix style

--- frontend/src/components/forms/ModalForm.tsx
+++ frontend/src/components/forms/ModalForm.tsx
@@ -32,8 +32,9 @@ export default function ModalForm({
             <div className="fixed inset-0 bg-black/25" />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <div className="">
+            {/* <div className="flex min-h-full items-center justify-center p-4 text-center"> */}
+            <div className="fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8 text-[#574938] shadow">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -43,7 +44,8 @@ export default function ModalForm({
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                {/* <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-gray p-6 text-left align-middle shadow-xl transition-all"> */}
+                <Dialog.Panel className="">
                   {/* <Dialog.Title
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
@@ -75,7 +77,7 @@ export default function ModalForm({
                     />
                   )}
 
-                  <div className="mt-4">
+                  {/* <div className="mt-4">
                     <button
                       type="button"
                       className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
@@ -83,8 +85,9 @@ export default function ModalForm({
                     >
                       Отмена
                     </button>
-                  </div>
+                  </div> */}
                 </Dialog.Panel>
+                
               </Transition.Child>
             </div>
           </div>
