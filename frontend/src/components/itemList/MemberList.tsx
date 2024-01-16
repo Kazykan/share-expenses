99b04634b04e88de6baf8f2e8ce873c1dc1b23e3
Rufat <rufat@kocherbaev.ru>
2024-01-16 14:32:15 +0300

    add icon & fix style

--- frontend/src/components/itemList/MemberList.tsx
+++ frontend/src/components/itemList/MemberList.tsx
@@ -1,16 +1,19 @@
-import { useUsersQuery } from "../../hooks/useUsersQuery"
 import { PlaceIdProps } from "../../interface"
 import { useMoneyTransfersQuery } from "../../hooks/useMoneyTransfersQuery"
 import currencyFormatMoney from "../../services/current.format.money"
 import { Member } from "../models/user.model"
 import { useExpensesQuery } from "../../hooks/useExpensesQuery"
-
-
+import { CiEdit, CiTrash } from "react-icons/ci"
+import { useUsersQuery } from "../../hooks/useUsersQuery"
 
 function MemberList({ placeId }: PlaceIdProps) {
   const placeIdNumber = Number(placeId)
 
-  const { data: dataUsers, isLoading, isSuccess } = useUsersQuery(placeIdNumber)
+  const {
+    data: dataUsers,
+    isLoading,
+    isSuccess,
+  } = useUsersQuery(placeIdNumber)
   const { data: dataMoneyTransfer } = useMoneyTransfersQuery(placeId)
   const { data: dataExpense } = useExpensesQuery(placeId)
 
@@ -66,42 +69,80 @@ function MemberList({ placeId }: PlaceIdProps) {
 
   return (
     <div className="px-4">
-      <ol className="relative border-s border-gray-200 dark:border-gray-700">
-        {isSuccess &&
-          dataUsers.map((placeUser: Member) => (
-            <>
-              <li className="mb-6 ms-4 pt-2" key={placeUser.id}>
-                <div className="mb-2 flex justify-between text-l font-semibold text-gray-900 dark:text-white">
-                  <div>{placeUser.username}&nbsp;</div>
-                  <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                    Удалить Ред.
-                  </div>
-                </div>
-                <div className="flex justify-between mb-2 text-sm font-normal text-gray-500 dark:text-gray-400">
-                  <div>
+      {/* <ol className="relative border-s border-gray-200 dark:border-gray-700"> */}
+      {isSuccess && (
+        <div className="py-3">
+          <div className="mx-auto max-w-sm space-y-4 rounded-lg p-4">
+            {dataUsers.map((member: Member) => (
+              <div
+                className="flex justify-between rounded-lg dark:bg-[#3D3A37] bg-white px-4 py-4 text-[#32371C] dark:text-[#D5D0CA] shadow"
+                key={member.id}
+              >
+                <div>
+                  <p>{member.username}</p>
+                  <p className="text-sm text-gray-500 dark:text-[#A7A29D]">
                     Потратил(a) всего:{" "}
-                    {currencyFormatMoney(userExpensesSum(placeUser.id!))}
-                    <p>
-                      Баланс:
-                      {BalanceUser(placeUser.id!) >= 0 ? (
-                        <div className="text-green-300">
-                          {currencyFormatMoney(BalanceUser(placeUser.id!))}
-                        </div>
-                      ) : (
-                        <div className="text-red-400">
-                          {currencyFormatMoney(BalanceUser(placeUser.id!))}
-                        </div>
-                      )}
-                    </p>
-                  </div>
+                    {currencyFormatMoney(userExpensesSum(member.id!))}
+                  </p>
+                  <p className="text-sm text-gray-500 dark:text-[#A7A29D]">
+                    Баланс:
+                    {BalanceUser(member.id!) >= 0 ? (
+                      <div className="text-lime-600">
+                        {currencyFormatMoney(BalanceUser(member.id!))}
+                      </div>
+                    ) : (
+                      <div className="text-orange-600">
+                        {currencyFormatMoney(BalanceUser(member.id!))}
+                      </div>
+                    )}
+                  </p>
                 </div>
-              </li>
-            </>
-          ))}
-      </ol>
+                <div>
+                  <button className="rounded p-2 hover:bg-gray-200 dark:focus:bg-[#597A7A] dark:hover:bg-[#8EBCBD]">
+                    <CiEdit />
+                  </button>
+                  <button className="rounded p-2 hover:bg-gray-200 dark:focus:bg-[#597A7A] dark:hover:bg-[#8EBCBD]">
+                    <CiTrash />
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      {/* // dataUsers.map((placeUser: Member) => (
+          //   <>
+          //     <li className="mb-6 ms-4 pt-2" key={placeUser.id}>
+          //       <div className="mb-2 flex justify-between text-l font-semibold text-gray-900 dark:text-white">
+          //         <div>{placeUser.username}&nbsp;</div>
+          //         <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
+          //           Удалить Ред.
+          //         </div>
+          //       </div>
+          //       <div className="flex justify-between mb-2 text-sm font-normal text-gray-500 dark:text-gray-400">
+          //         <div>
+          //           Потратил(a) всего:{" "}
+          //           {currencyFormatMoney(userExpensesSum(placeUser.id!))}
+          //           <p>
+          //             Баланс:
+          //             {BalanceUser(placeUser.id!) >= 0 ? (
+          //               <div className="text-green-300">
+          //                 {currencyFormatMoney(BalanceUser(placeUser.id!))}
+          //               </div>
+          //             ) : (
+          //               <div className="text-red-400">
+          //                 {currencyFormatMoney(BalanceUser(placeUser.id!))}
+          //               </div>
+          //             )}
+          //           </p>
+          //         </div>
+          //       </div>
+          //     </li>
+          //   </>
+          // ))}
+      // </ol> */}
     </div>
   )
 }
 
 export default MemberList
-
