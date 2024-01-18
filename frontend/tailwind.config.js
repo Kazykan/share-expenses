3cb54ab873dd78c8e6f9a99330ecce0b7d779911
Rufat <rufat@kocherbaev.ru>
2024-01-18 03:09:05 +0300

    add packeges

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,8 @@
+const plugin = require("tailwindcss/plugin")
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
@@ -11,5 +10,8 @@ export default {
     // require('@headlessui/tailwindcss')
 
     // Or with a custom prefix:
-    require('@headlessui/tailwindcss')({ prefix: 'ui' })],
+    require("@headlessui/tailwindcss")({ prefix: "ui" }),
+    require("tailwindcss-react-aria-components"),
+    require("tailwindcss-animate"),
+  ],
 }
